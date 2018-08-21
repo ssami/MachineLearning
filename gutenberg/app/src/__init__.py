de00918dd126845c09ca56090b02f36835a64f2a
ssami <ssami@ebay.com>
2018-08-20 22:07:09 -0700

    Fixing decode problem and removing unused model upload

--- gutenberg/app/src/__init__.py
+++ gutenberg/app/src/__init__.py
@@ -5,6 +5,7 @@ from src import constants
 from src.model import ModelInfo
 from src.exceptions import InvalidDataError
 from src.utils import allowed_files
+import json
 
 import os
 
@@ -21,50 +22,30 @@ def main():
     return "Hello, world!"
 
 
-@app.route("/model", methods=["POST", "GET"])
-def model():
+@app.route("/model", methods=["PUT", "GET"])
+def all_models():
     if request.method == "GET":
-        model_list = [x.decode('utf-8') for x in conn.find(constants.MODEL_KEYSPACE)]
-        return jsonify(model_list), 200
+        resp = [x for x in conn.findall(constants.MODEL_KEYSPACE)]
+        return jsonify(resp), 200
     else:
         if request.is_json:
             data = request.get_json()
-            model_info = ModelInfo(data['description'], data['metrics'], None)
+            model_info = ModelInfo(data['description'], data['metrics'], data['location'])
             try:
-                conn.store(constants.MODEL_KEYSPACE, model_info.hash)
+                # todo: fix how we store a dict of metrics
                 conn.store(model_info.hash, model_info.items)
             except Exception as e:
                 return jsonify(str(e)), 500
-            return jsonify("Success"), 201
+            return jsonify(model_info.hash), 201
         else:
             return jsonify(InvalidDataError("request is not in JSON")), 500
 
 
-@app.route("/model_upload", methods=["POST", "GET"])
-def model_upload():
-    app.config['UPLOAD_CONSTANTS'] = constants.UPLOADS_FOLDER
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return "No file uploaded", 400
-        fl = request.files['file']
-        if fl and allowed_files(fl.filename, constants.ALLOWED_EXTENSIONS):
-            filename = secure_filename(fl.filename)
-            try:
-                fl.save(os.path.join(app.instance_path, filename))
-                return jsonify("File successfully uploaded!"), 200
-            except Exception as e:
-                return jsonify(str(e)), 500
-        return 'No file or unaccepted format', 400
-
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+@app.route("/model/<modelId>")
+def model(modelId):
+    info_dict = conn.find(modelId)
+    met = json.loads(info_dict['metrics'])
+    return jsonify(info_dict)
 
 
 if __name__ == "__main__":
